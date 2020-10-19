1436bfa6de0ca81381363e5dc32636f9f1153012
jeben-hub <abondaronok@gmail.com>
2020-10-19 15:05:41 +0300

    feat: add toReadable function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function toReadable (number) {
-  
+  if (number == 0) return "zero";
+  var dg = ['','one','two','three','four', 'five','six','seven','eight','nine'];
+  var tn = ['ten','eleven','twelve','thirteen', 'fourteen','fifteen','sixteen', 'seventeen','eighteen','nineteen'];
+  var tw = ["", "", 'twenty','thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  let flag = false;
+  let str = number.toString().split("");
+  while (str.length < 3){
+  	str.splice(0, 0, "0");
+  }
+  return  str.map((char,index) => {
+  	let num = Number.parseInt(char);
+  	switch (index) {
+	  	case 2:
+		  	if (flag){
+	  			flag = false;
+	  			return tn[num];
+	  		}
+	  		return dg[num];
+	  	case 1:
+	  		if (num == 0)
+	  			return "";
+	  		else if (num == 1){
+	  			flag = true;
+	  			return "";
+	  		}
+	  		return tw[num];
+	  	case 0:
+	   		return num == 0 ? "" : dg[num] + " hundred";
+	  }
+  }).filter(el => el).join(" ").trim();
 }
